c436c2f229ce206c375d0636f006bff6ae8a14d0
KaiAtDCI <kaiatdci@web.de>
2022-06-20 10:06:44 +0200

    deleted subcomponents

--- spring/crud-spring-react/frontend/src/App.js
+++ spring/crud-spring-react/frontend/src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
-import './App.css';
-import Home from './components/Home.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ClientList from './components/ClientList.js';
-import ClientEdit from "./components/ClientEdit.js";
 
 class App extends Component {
-  render() {
-    return (
-        // todo: make router work, url is adjusted but not served, use hash-router instead?
-        <Router>
-          <Switch>
-            <Route path='/' exact={true} component={Home}/>
-            <Route path='/clients' exact={true} component={ClientList}/>
-            <Route path='/clients/:id' component={ClientEdit}/>
-          </Switch>
-        </Router>
-    )
-  }
-}
+    state = {
+        clients: []
+    };
 
+    async componentDidMount() {
+        const response = await fetch('/clients');
+        const body = await response.json();
+        this.setState({clients: body});
+    }
+
+    render() {
+        const {clients} = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="App-intro">
+                        <h2>Clients</h2>
+                        {clients.map(client =>
+                            <div key={client.id}>
+                                {client.name} ({client.email})
+                            </div>
+                        )}
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
 export default App;
